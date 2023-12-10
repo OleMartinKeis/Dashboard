aa9de5c7666075c439b0c76afbe8e23ddb0191df
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-12-10 19:28:11 +0100

    Created navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { SearchIcon } from "@heroicons/react/solid";
+import { TextInput } from "@tremor/react";
+
+function Navbar() {
+    return (
+        <div
+            id="top"
+            className="relative w-full sm:flex justify-between items-center p-2"
+        >
+            <h1 className="font-bold ">Dashboard</h1>
+            <div className="py-2">
+                <TextInput icon={SearchIcon} placeholder="Search..." />
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
