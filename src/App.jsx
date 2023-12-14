b30882c7758dc8ee547643e390164934975e0e6b
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-12-14 09:04:39 +0100

    Added sidebar and leftColumn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import LeftColumn from "./components/leftColumn";
+import Navbar from "./components/navbar";
+import RightColumn from "./components/rightColumn";
+import Sidebar from "./components/sidebar";
 
 function App() {
-    return <></>;
+    return (
+        <div className="flex bg-background text-white">
+            <Sidebar />
+            <div className="flex flex-col flex-1 relative">
+                <Navbar />
+
+                <div className="grid md:grid-cols-3 grid-cols-1 w-full">
+                    <div className="col-span-2">
+                        <LeftColumn />
+                    </div>
+                    <div className="w-full">
+                        <RightColumn />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
